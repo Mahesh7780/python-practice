2e5daa1d3b52c5cc8f50912eeebf2420128f3def
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-02-25 23:46:41 +0530

    Add various star pattern printing functions

--- patterns.py
+++ patterns.py
@@ -52,3 +52,106 @@ for i in range(n):
   * * * 
    * * 
     * 
+
+#down hill
+for i in range(n):
+    for j in range(i):
+        print("",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+    
+#output
+
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+
+# hill pattern
+for i in range(n):
+    for j in range(i,n):
+        print("",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+#output
+
+    * 
+   * * 
+  * * * 
+ * * * * 
+
+#left side
+
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+#output
+
+  * * * * * 
+    * * * * 
+      * * * 
+        * * 
+          * 
+# right side 
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+
+#output
+
+          * 
+        * * 
+      * * * 
+    * * * * 
+  * * * * * 
+
+# decreasing triangle
+for i in range(n):
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+
+#output
+
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+#increasing triangle
+for i in range(n):
+    for i in range(i+1):
+        print("*",end=" ")
+    print()
+
+#output
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+#rectangle   
+for i in range(n):
+    for j in range(n):
+        print("*",end=" ")
+    print()
+    
+#output
+
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
