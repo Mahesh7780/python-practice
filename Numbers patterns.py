e02888bff7f6f6c37019f656448963766c1721e4
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-03-05 12:38:16 +0530

    Update Numbers patterns.py

--- Numbers patterns.py
+++ Numbers patterns.py
@@ -44,3 +44,22 @@ for i in range(n):
       3 3 3 
     4 4 4 4 
   5 5 5 5 5 
+
+#reverse hill
+n=5
+p=5
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n-1):
+        print(p,end=" ")
+    for j in range(i,n):
+        print(p,end=" ")
+    p-=1
+    print()
+#O/P
+  5 5 5 5 5 5 5 5 5 
+    4 4 4 4 4 4 4 
+      3 3 3 3 3 
+        2 2 2 
+          1 
