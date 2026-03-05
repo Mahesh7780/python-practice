01e8cefc53c5dd42b158a585a5352321aa4ae31f
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-03-05 12:30:10 +0530

    Update Numbers patterns.py

--- Numbers patterns.py
+++ Numbers patterns.py
@@ -15,3 +15,21 @@ for i in range(n):
         print(p,end=" ")
     p-=1
     print()
+
+# right side triangle of nums
+n=5
+p=1
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print(p,end=" ")
+    p+=1
+    print()
+
+#O/p
+          1 
+        2 2 
+      3 3 3 
+    4 4 4 4 
+  5 5 5 5 5 
