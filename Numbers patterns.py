bd2f21d1474f2ec42ccf5ed4e02cbdef1983cf53
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-03-05 12:44:35 +0530

    Update Numbers patterns.py

--- Numbers patterns.py
+++ Numbers patterns.py
@@ -82,3 +82,37 @@ for i in range(n):
       3 3 3 3 3 
     2 2 2 2 2 2 2 
   1 1 1 1 1 1 1 1 1 
+
+# diamond pattern
+n=5
+p=1
+for i in range(n-1):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print(p,end=" ")
+    for j in range(i):
+        print(p,end=" ")
+    p+=1
+    print()
+p=5
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n-1):
+        print(p,end=" ")
+    for j in range(i,n):
+        print(p,end=" ")
+    p-=1
+    print()
+    
+#O/P
+          1 
+        2 2 2 
+      3 3 3 3 3 
+    4 4 4 4 4 4 4 
+  5 5 5 5 5 5 5 5 5 
+    4 4 4 4 4 4 4 
+      3 3 3 3 3 
+        2 2 2 
+          1 
