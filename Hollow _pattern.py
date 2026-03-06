e1817d367b871b7ca13d2563fc38a628181f2ed8
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-03-06 23:13:10 +0530

    Update Hollow _pattern.py

--- Hollow _pattern.py
+++ Hollow _pattern.py
@@ -66,3 +66,25 @@ for i in range(n):
 *   * 
 *     * 
 * * * * *
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i):
+        if i==0 or j==0 or i==n-1 or j==n-1 or i==j:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(i+1):
+        if  i==n-1 or j==i:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+    
+#O/P
+          * 
+        *   * 
+      *       * 
+    *           * 
+  * * * * * * * * * 
