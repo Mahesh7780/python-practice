d4d17c1b1c40deb6183c3f3cecc769f7760bf7e8
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-05-13 17:34:44 +0530

    Update maths.py

--- maths.py
+++ maths.py
@@ -267,6 +267,19 @@ while b!=0:
     a=tep
 print(a)
 
+#or
+a=12
+b=18
+g=0
+if a<b:
+    l=a
+else:
+    l=b
+for i in range(1,l+1):
+    if a%i==0 and b%i==0:
+        g=i
+print(g)
+
 #Power of a number (x^y)
 x=2
 y=3
