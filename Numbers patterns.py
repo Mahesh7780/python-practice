973c4b85632e7a42ed0df91cc2160f01988be260
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-05-16 15:57:31 +0530

    Implement concentric square numbers pattern
    Added code to generate a concentric square number pattern.

--- Numbers patterns.py
+++ Numbers patterns.py
@@ -1,3 +1,28 @@
+
+# Concentric Square Numbers
+n = 5
+size = 2 * n - 1
+
+for i in range(size):
+
+    for j in range(size):
+
+        print(n - min(i, j, size - 1 - i, size - 1 - j), end=" ")
+
+    print()
+    
+    #o/p
+    
+5 5 5 5 5 5 5 5 5 
+5 4 4 4 4 4 4 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 2 1 2 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 4 4 4 4 4 4 5 
+5 5 5 5 5 5 5 5 5 
+
 # Double Number Pyramid
 n=5
 for i in range(n):
