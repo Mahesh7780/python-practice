7a404699dea56e697e7713f9ccd22d64334e9ae7
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-05-16 12:19:58 +0530

    Add Pascal's Triangle implementation
    Implement Pascal's Triangle generation in Python.

--- Numbers patterns.py
+++ Numbers patterns.py
@@ -1,3 +1,22 @@
+
+# Pascal’s Triangle
+n=5
+for i in range(n):
+    num=1
+    for j in range(i,n):
+        print("",end=" ")
+    for j in range(i+1):
+        print(num,end=" ")
+        
+        num=num*(i-j)//(j+1)
+    print()
+#o/p
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+ 1 4 6 4 1 
+
 #increasing nums
 n=5
 p=1
